b899b072380f4bce85930b6a0b642c9827298b9d
Miscoci Luca Laurentiu <lmiscoci@softjourn.com>
2023-11-21 10:10:20 +0200

    layout component refactor code

--- platomics-angular-challenge/src/app/layouts/layout/layout.component.ts
+++ platomics-angular-challenge/src/app/layouts/layout/layout.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
-import { VexConfigService } from '../../../theme/@vex/config/vex-config.service';
-import { AsyncPipe, NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgTemplateOutlet } from '@angular/common';
 import { BaseLayoutComponent } from '../base-layout/base-layout.component';
-import { VexConfig } from '../../../theme/@vex/config/vex-config.interface';
 import {HeaderComponent} from "../header/header.component";
 @Component({
   selector: 'vex-layout',
@@ -12,8 +9,6 @@ import {HeaderComponent} from "../header/header.component";
   styleUrls: ['./layout.component.scss'],
     imports: [
         BaseLayoutComponent,
-        NgIf,
-        AsyncPipe,
         NgTemplateOutlet,
         RouterOutlet,
         HeaderComponent,
@@ -21,8 +16,5 @@ import {HeaderComponent} from "../header/header.component";
   standalone: true
 })
 export class LayoutComponent {
-  config$: Observable<VexConfig> = this.configService.config$;
-  constructor(
-    private readonly configService: VexConfigService
-  ) {}
+
 }
